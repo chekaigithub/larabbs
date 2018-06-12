debc3a12722db8df6970bedb199eab8077968a4d
chekaigithub <2485611545@qq.com>
2018-06-12 02:18:15 +0000

    update

--- config/app.php
+++ config/app.php
@@ -179,6 +179,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Frozennode\Administrator\AdministratorServiceProvider::class,
+
     ],
 
     /*
